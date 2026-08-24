5264fcb4913769a8a63d9459c3d1c41e3a0ac135
Sarai <sarai@MacBook-Neo-de-Sarai.local>
2026-08-24 14:57:36 +0200

    Complete Step 1 deposit class

---
+++ index.js
@@ -0,0 +1,35 @@
+let balance = 500.0;
+
+class Withdrawal {
+  constructor(amount) {
+    this.amount = amount;
+  }
+
+  commit() {
+    balance -= this.amount;
+  }
+}
+
+class Deposit {
+  constructor(amount) {
+    this.amount = amount;
+  }
+
+  commit() {
+    balance += this.amount;
+  }
+}
+
+const t1 = new Withdrawal(50.25);
+t1.commit();
+console.log("Transaction 1:", t1);
+
+const t2 = new Withdrawal(9.99);
+t2.commit();
+console.log("Transaction 2:", t2);
+
+const t3 = new Deposit(120.0);
+t3.commit();
+console.log("Transaction 3:", t3);
+
+console.log("Balance:", balance);
