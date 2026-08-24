76fc0c35fdef1ada22259468f277fb5654b48236
Sarai <sarai@MacBook-Neo-de-Sarai.local>
2026-08-24 15:06:02 +0200

    Complete Step 5 refactor commit

--- index.js
+++ index.js
@@ -10,17 +10,21 @@ class Transaction {
     this.account = account;
     this.amount = amount;
   }
+
+  commit() {
+    this.account.balance += this.value;
+  }
 }
 
 class Withdrawal extends Transaction {
-  commit() {
-    this.account.balance -= this.amount;
+  get value() {
+    return -this.amount;
   }
 }
 
 class Deposit extends Transaction {
-  commit() {
-    this.account.balance += this.amount;
+  get value() {
+    return this.amount;
   }
 }
 
@@ -31,16 +35,19 @@ const myAccount = new Account("snow-patrol");
 const t1 = new Withdrawal(myAccount, 50.25);
 t1.commit();
 console.log("Transaction 1:", t1);
+console.log("Value 1:", t1.value);
 
 // Transaction 2 - Withdrawal
 const t2 = new Withdrawal(myAccount, 9.99);
 t2.commit();
 console.log("Transaction 2:", t2);
+console.log("Value 2:", t2.value);
 
 // Transaction 3 - Deposit
 const t3 = new Deposit(myAccount, 120.0);
 t3.commit();
 console.log("Transaction 3:", t3);
+console.log("Value 3:", t3.value);
 
 // Final account balance
 console.log("Balance:", myAccount.balance);
